3100ef7bb43f752074ce6ffcccc213b4f219373a
Muskan Kumari <muskangen1395@gmail.com>
2025-09-05 23:56:32 +0530

    #

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,8 @@ import cors from "cors"
 const app = express()
 const port = process.env.PORT || 5000
 app.use(cors({
-    origin
+    origin:"http://localhost:5173",
+    credentials:true
 }))
 app.use(express.json())
 app.use(cookieParser())
